449cb07ca68f899dfdf1a4659428010718c4d118
Xindi LI <xil573@eng.ucsd.edu>
2017-01-26 10:37:53 -0800

    lab3

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -11,8 +11,24 @@ $(document).ready(function() {
 function initializePage() {
 	$("#testjs").click(function(e) {
 		$('.jumbotron h1').text("Javascript is connected");
+		$("#testjs").text("Please wait...");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+function projectClick(e){
+	e.preventDefault();
+	var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       //description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+       //$(".project-description").fadeToggle( "slow", "linear" );
+       $(".project-description").fadeOut();
+    }
 }
